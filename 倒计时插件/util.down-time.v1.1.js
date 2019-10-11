45b3b6c55a9d9fbbab8f901a93de71ae15f79df6
ken <34732564+ten-ken@users.noreply.github.com>
2019-10-11 11:13:21 +0800

    Update util.down-time.v1.1.js

--- 倒计时插件/util.down-time.v1.1.js
+++ 倒计时插件/util.down-time.v1.1.js
@@ -58,4 +58,44 @@ _DownTime.prototype = {
 		}, 1000);
 		
 	},
-	timeFormat: function(param) { //小于1
+	timeFormat: function(param) { //小于10的格式化函数
+		return param < 10 ? '0' + param : param;
+	},
+	setHight:function(len){//设置高度 给移动端适配 如默认单位高度是412 不合适 重写方法setHight 在函数参数里面
+		// console.log(len);
+		return len * 412;
+	},
+	handSeckill:function(o,newTime,_this) {
+		let endTime = null;
+		if (o.isStart == 1) {
+			endTime = new Date(o[_this.end].replace(/-/g, "/")).getTime();
+		} else {
+			endTime = new Date(o[_this.start].replace(/-/g, "/")).getTime();
+		}
+		let obj = null;
+		// 如果活动未结束，对时间进行处理
+		if (endTime - newTime > 0) {
+			let time = (endTime - newTime) / 1000;
+			// 获取天、时、分、秒
+			let day = parseInt(time / (60 * 60 * 24));
+			// console.log(time);
+			let hour = parseInt(time % (60 * 60 * 24) / 3600);
+			let min = parseInt(time % (60 * 60 * 24) % 3600 / 60);
+			let sec = parseInt(time % (60 * 60 * 24) % 3600 % 60);
+			obj = {
+				day: _this.timeFormat(day),
+				hour: _this.timeFormat(hour),
+				min: _this.timeFormat(min),
+				sec: _this.timeFormat(sec)
+			}
+		} else { //活动已结束，全部设置为'00'
+			obj = {
+				day: '00',
+				hour: '00',
+				min: '00',
+				sec: '00'
+			}
+		}
+		o.downTimeArr = obj;
+	},
+}
